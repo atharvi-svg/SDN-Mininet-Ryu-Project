046664ddb6c94a93a815db95345afed4b93c3f0a
Atharvi Desurkar <atharvidesurkar@gmail.com>
2026-04-16 22:41:22 +0530

    Add files via upload

---
+++ controller (1).py
@@ -0,0 +1,60 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet
+
+
+class SimpleFirewall(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleFirewall, self).__init__(*args, **kwargs)
+
+    #  Install default rule
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
+
+        inst = [parser.OFPInstructionActions(
+            ofproto.OFPIT_APPLY_ACTIONS, actions)]
+
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=0,
+            match=match,
+            instructions=inst
+        )
+        datapath.send_msg(mod)
+
+    #  FINAL WORKING LOGIC
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
+
+        in_port = msg.match['in_port']
+
+        #BLOCK ALL TRAFFIC FROM h1 (PORT 1)
+        if in_port == 1:
+            print("BLOCKING TRAFFIC FROM h1")
+            return
+
+        # ALLOW OTHERS
+        actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=msg.data
+        )
+        datapath.send_msg(out)
